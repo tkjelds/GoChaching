2809d2b503e104b3bdf430354cd8277536aafc27
tokj <60293230+tokj@users.noreply.github.com>
2021-03-12 19:38:35 +0100

    redid adapter again

--- app/src/main/java/dk/itu/moapd/gocaching/GeoCacheArrayAdapter.kt
+++ app/src/main/java/dk/itu/moapd/gocaching/GeoCacheArrayAdapter.kt
@@ -9,10 +9,10 @@ import android.widget.TextView
 
 class GeoCacheArrayAdapter(context: Context, geoCaches:List<GeoCache>) :
         ArrayAdapter<GeoCache>(context,R.layout.list_geo_cache,geoCaches) {
-    private class GeoCacheViewHolder {
-        var cache: TextView? = null
-        var where: TextView? = null
-        var date: TextView? = null
+    private class GeoCacheViewHolder(view : View) {
+        var cache = view.findViewById<View>(R.id.cache_text) as TextView
+        var where = view.findViewById<View>(R.id.where_text) as TextView
+        var date  = view.findViewById<View>(R.id.date_text) as TextView
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
@@ -21,17 +21,14 @@ class GeoCacheArrayAdapter(context: Context, geoCaches:List<GeoCache>) :
         if (view == null)  {
             val inflater = LayoutInflater.from(context)
             view = inflater.inflate(R.layout.list_geo_cache, parent, false)
-            viewHolder = GeoCacheViewHolder()
-            viewHolder.cache = view.findViewById<View>(R.id.cache_text) as TextView
-            viewHolder.where = view.findViewById<View>(R.id.where_text) as TextView
-            viewHolder.date  = view.findViewById<View>(R.id.date_text) as TextView
+            viewHolder = GeoCacheViewHolder(view)
                             }
         else
             viewHolder = view.tag as GeoCacheViewHolder
         val geoCache = getItem(position)
-        viewHolder.cache!!.text = geoCache?.cache
-        viewHolder.where!!.text = geoCache?.where
-        viewHolder.date!!.text = geoCache?.date.toString()
+        viewHolder.cache.text = geoCache?.cache
+        viewHolder.where.text = geoCache?.where
+        viewHolder.date.text = geoCache?.date.toString()
         view?.tag = viewHolder
         return view!!
     }
